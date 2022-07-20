e7a23e593bb39109b094eb15dc6b49d712fe3cba
Dewmith Akalanka <dewmitha@fortude.co>
2022-07-20 18:53:38 +0530

    Linked Factorial API in Fibonacci API using promised based http requests [offline]

--- src/functions/fibonacci/handler.ts
+++ src/functions/fibonacci/handler.ts
@@ -1,13 +1,15 @@
 import { successResponse, serverErrorResponse } from "@libs/api-gateway";
 import { APIGatewayProxyEvent } from "aws-lambda";
 import { fibonacci as fib } from "@ultirequiem/fibonacci";
+import axios from "axios";
 
 const fibonacci = async (e: APIGatewayProxyEvent) => {
   const input = e.queryStringParameters?.number;
 
   const host = e.headers.Host;
-  const baseURL = `https://${host}/dev`;
+  const baseURL = `http://${host}/dev`;
 
+  //Fibonacci Calculation
   if (input == null || input === "" || isNaN(Number(input))) {
     return serverErrorResponse({
       message: "Missing number parameter",
@@ -18,15 +20,26 @@ const fibonacci = async (e: APIGatewayProxyEvent) => {
 
   if (number < 0 || number > 100) {
     return serverErrorResponse({
-      message: "Number must be greater than 0 and less than 100",
+      message: "Number must be greater than 0 and less than 150",
     });
   }
 
   const fibonacci = await fib(number);
+  let factorialValue: any;
+
+  try {
+    const factorial = await axios.get(`${baseURL}/factorial/?number=${number}`);
+    factorialValue = factorial.data.factorial;
+  } catch (error) {
+    return serverErrorResponse({
+      message: "Connecting to Factorial API failed!" + error,
+    });
+  }
 
   return successResponse({
     input: input,
     fibonacci: fibonacci,
+    factorialValue,
   });
 };
 
